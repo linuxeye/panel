b8935674eaf6c2efbc3bd2bdd7cdcbd6bbaab49f
lj2007331@gmail.com <lj2007331@gmail.com>
2018-01-27 22:25:51 +0800

    Update sqlite3 PATH

--- panel/settings.py
+++ panel/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'panel.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'data/panel.db'),
     }
 }
 
