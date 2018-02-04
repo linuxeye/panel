3021f77ed1303ab0e5e52d1eb8bcb40f00b57b9a
lj2007331@gmail.com <lj2007331@gmail.com>
2018-02-04 23:28:27 +0800

    Update ftp

--- ftp/urls.py
+++ ftp/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^reload$', views.reload),
+    url(r'^delete$', views.deleteuser),
+    url(r'^setpassword$', views.setpassword),
+    url(r'^setstatus$', views.setstatus),
 ]
--- ftp/views.py
+++ ftp/views.py
@@ -5,19 +5,58 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 from django.conf import settings
-import os
+import os,json
+from django.core import serializers
 #from ftp.forms import *
-#from ftp.models import *
+from ftp.models import *
 
 @login_required
 def reload(request):
     os.system(settings.OPTIONS['pureftpd_install_dir'] + '/bin/pure-pw mkdb')
     return HttpResponse(settings.OPTIONS)
 
+def index(request):
+    dataset = User.objects.filter().all()
+    data0 = serializers.serialize("json", dataset) 
+    content = list()
+    for i in json.JSONDecoder().decode(data0):
+        j = i['fields']
+        j['id'] = i['pk']
+        content.append(j)
+    #print(content)
+    return render(request, "ftp.html", { "content": content})
+
 #def adduser(request):
 
-#def deleteuser(request):
+def deleteuser(request):
+    try:
+        post = json.loads(request.body)
+        User.objects.filter(id=post['id']).delete()
+        content = { 'flag': 'Success' }
+    except Exception as e:
+        content = { 'flag': 'Error', 'context': str(e) }
+    return JsonResponse(content)
 
-#def resetpassword(request):
+@login_required
+def setpassword(request):
+    try:
+        post = json.loads(request.body)
+        username = User.objects.get(id=post['id']) 
+        new_password=post['password']
+        User.objects.filter(id=post['id']).update(password=post['password'])
+        print(settings.OPTIONS['pureftpd_install_dir'] + '/bin/pure-pw passwd ' + username + '<<EOF \n' + new_password + '\n' + new_password + '\nEOF')
+        #os.system(settings.OPTIONS['pureftpd_install_dir'] + '/bin/pure-pw passwd ' + username + '<<EOF \n' + new_password + '\n' + new_password + '\nEOF')
+        content = { 'flag': 'Success' }
+    except Exception as e:
+        content = { 'flag': 'Error', 'context': str(e) }
+    return JsonResponse(content)
 
-#def setstatus(request):
+@login_required
+def setstatus(request):
+    try:
+        post = json.loads(request.body)
+        User.objects.filter(id=post['id']).update(status=post['status'])
+        content = { 'flag': 'Success' }
+    except Exception as e:
+        content = { 'flag': 'Error', 'context': str(e) }
+    return JsonResponse(content)
