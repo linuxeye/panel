62232a6f638b19d9510734595c0f7ba8e57348db
sudi <i-disu@pptv.com>
2018-12-18 15:14:29 +0800

    update databases models

--- database/models.py
+++ database/models.py
@@ -3,7 +3,7 @@ from website.models import Website
 
 # Create your models here.
 class Database(models.Model):
-    website = models.ForeignKey(Website, on_delete=models.CASCADE)
+    website = models.ForeignKey(Website, null=True, on_delete=models.CASCADE)
     dbname = models.CharField(max_length=50, unique=True)
     dbuser = models.CharField(max_length=50, unique=True)
     dbhost = models.CharField(max_length=50)
