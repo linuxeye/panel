8d80668922bbf93e9c80583c2290f62b984f2bc4
lj2007331@gmail.com <lj2007331@gmail.com>
2018-12-30 10:40:18 +0800

    Update panel pid

--- runconfig.py
+++ runconfig.py
@@ -8,14 +8,13 @@ sys.path.insert(0, path_of_current_dir)
 workers = 1
 threads = 1
 backlog = 512
-daemon = True
 timeout = 30
 bind = "%s:%s" % ("0.0.0.0", public.readfile('data/port.conf'))
 worker_class = 'gevent'
-chdir = path_of_current_dir 
+chdir = path_of_current_dir
 capture_output = True
 access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'
 loglevel = 'info'
-pidfile = '%s/logs/%s.pid' % (path_of_current_dir, _file_name)
+pidfile = '/var/run/panel.pid'
 errorlog = '%s/logs/%s_error.log' % (path_of_current_dir, _file_name)
 accesslog = '%s/logs/%s_access.log' % (path_of_current_dir, _file_name)
