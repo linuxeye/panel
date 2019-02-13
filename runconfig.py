df62b47fb9a52650e8dfebade87bbddb06cdf8b1
yeho <lj2007331@gmail.com>
2019-02-13 15:43:30 +0800

    Update runconfig.py

--- runconfig.py
+++ runconfig.py
@@ -3,7 +3,6 @@ import sys
 from libs import public
 path_of_current_file = os.path.abspath(__file__)
 path_of_current_dir = os.path.split(path_of_current_file)[0]
-_file_name = os.path.basename(__file__)
 sys.path.insert(0, path_of_current_dir)
 workers = 1
 threads = 1
@@ -16,5 +15,5 @@ capture_output = True
 access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'
 loglevel = 'info'
 pidfile = '/var/run/panel.pid'
-errorlog = '%s/logs/%s_error.log' % (path_of_current_dir, _file_name)
-accesslog = '%s/logs/%s_access.log' % (path_of_current_dir, _file_name)
+errorlog = '%s/logs/panel_error.log' % path_of_current_dir
+accesslog = '%s/logs/panel_access.log' % path_of_current_dir
